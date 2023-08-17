1a534882282ebe3382591f9d4cd88910a7a0e3d9
SCCMOG <rkcsjsy@gmail.com>
2023-08-17 16:52:01 +0200

    nginx

--- radarr-proxy/entrypoint.sh
+++ radarr-proxy/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tempio -conf /data/options.json -template /nginx.conf.gtpl -out /tmp/nginx.conf
+tempio -conf /data/options.json -template /nginx.gtpl -out /tmp/nginx.conf
 nginx -T -c /tmp/nginx.conf
 
 exec nginx -c /tmp/nginx.conf
--- radarr-proxy/nginx.gtpl
+++ radarr-proxy/nginx.gtpl
@@ -1,5 +1,3 @@
-
-
 # Run nginx in foreground.
 daemon off;
 
